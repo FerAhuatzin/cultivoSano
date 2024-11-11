199598c8e4344a1f4f9500e23a4d235e13285b4f
Fer Ahuatzin <64028335+FerAhuatzin@users.noreply.github.com>
2024-11-10 21:03:39 -0600

    Revert "tree?"
    This reverts commit 2bddf24ed90bb1316bc82e3585e4bdc3a7c19199.

--- Cultivo_Sano/Cultivo_Sano/Cultivo_SanoApp.swift
+++ Cultivo_Sano/Cultivo_Sano/Cultivo_SanoApp.swift
@@ -1,6 +1,6 @@
 //
-//  Cultivos_SanoApp.swift
-//  Cultivos_Sano
+//  Cultivo_SanoApp.swift
+//  Cultivo_Sano
 //
 //  Created by iOS Lab on 10/11/24.
 //
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct Cultivos_SanoApp: App {
+struct Cultivo_sanoApp: App {
     var body: some Scene {
         WindowGroup {
             Login()
